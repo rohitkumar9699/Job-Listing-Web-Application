1125b72b81614f665fc2eb6178fbcc62512e4638
Rohit Kumar <rohitkumar@srmap.edu.in>
2025-04-05 16:03:13 +0530

    change in schema

--- backend/models/Job.js
+++ backend/models/Job.js
@@ -1,13 +1,19 @@
 const mongoose = require("mongoose");
 
 const jobSchema = new mongoose.Schema({
-  title: String,
-  location: String,
-  description: String,
-  employmentType: String,
-  postedDate: Date,
-  source: String,
-  experienceRange: String,
+  jobId: { type: String },
+  title: { type: String, required: true },
+  company: { type: String },
+  location: { type: String },
+  job_link: { type: String },
+  employment_type: { type: String },
+  experience: { type: String },
+  source: { type: String },
+  country: { type: String },
+  postedDateTime: { type: Date },
+  companyImageUrl: { type: String },
+  min_exp: { type: Number },
+  max_exp: { type: Number },
 });
 
 module.exports = mongoose.model("Job", jobSchema);
